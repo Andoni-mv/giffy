d14e3a4e113a49206a32206256de3855b299acfc
Andoni <and.moreno18@gmail.com>
2021-03-20 20:38:53 +0100

    seo-and-github-integration

---
+++ src/pages/SearchResults/index.js
@@ -0,0 +1,40 @@
+import React, {useEffect, useRef, useCallback} from 'react'
+import Spinner from 'components/Spinner'
+import ListOfGifs from 'components/ListOfGifs'
+import {useGifs} from 'hooks/useGifs'
+import useNearScreen from 'hooks/useNearScreen'
+import debounce from "just-debounce-it"
+import {Helmet} from 'react-helmet'
+
+
+export default function SearchResults ({ params }) {
+  const { keyword } = params
+  const { loading, gifs, setPage } = useGifs({ keyword })
+  const visorRef = useRef()
+  const {isNearScreen} = useNearScreen({ externalRef: !loading && visorRef, once: false })
+
+  const debounceNextPage = useCallback(debounce(
+    () => setPage(prevPage => prevPage + 1), 2000
+  ), [])
+
+  useEffect(function () {
+    if (isNearScreen) debounceNextPage()
+  })
+
+  return <>
+    {loading
+      ? <Spinner />
+      : <>
+          <Helmet>
+            <title>{keyword} | Giffy</title>  
+            <meta name="description" content={keyword}></meta>
+          </Helmet> 
+        <h3 className="App-title">
+          {decodeURI(keyword)}
+        </h3>
+        <ListOfGifs gifs={gifs} />
+        <div data-testid="visor" ref={visorRef} />
+      </>
+    }
+  </>
+}
